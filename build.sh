0bd7419fa46ec579a14b8a46051afdb948513afa
Avram Lyon <ajlyon@gmail.com>
2011-09-11 21:35:17 -0700

    Excluding build scripts from XPI packager

--- build.sh
+++ build.sh
@@ -6,5 +6,7 @@ find . -name '.hg' -prune -o \
        -name '.git' -prune -o \
        -name '.hgignore' -prune -o \
        -name '.xpi' -prune -o \
+       -name 'fw-update.sh' -prune -o \
+       -name 'build.sh' -prune -o \
        -name '*~' -prune -o \
        -print | xargs zip scaffold.xpi
