26c787e1c78a59bfcca1a5c98380129f46d9eb91
Aurimas Vinckevicius <aurimas.dev@gmail.com>
2016-04-10 14:13:20 -0400

    Prompt to update min/maxVersions when creating a release

--- release.sh
+++ release.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+# Make sure local tags are up to date with github repo
+git fetch -t "https://github.com/zotero/scaffold.git"
+
+lastTag="`git describe --tags --abbrev=0 --match=v[0-9]*`"
+
 version="$1"
 if [ -z "$version" ]; then
-	read -p "Version number: " version
+	echo "Last version: $lastTag" 1>&2
+	read -p " New version: v" version
 fi
 
 srcPath="src"
@@ -13,7 +19,6 @@ srcPath="src"
 changeLog="Changelog"
 tempFile="`mktemp`"
 tempFile2="`mktemp`"
-lastTag="`git tag -l v[0-9]* | tail -n 1`"
 
 git log --reverse --no-merges --format="* %s" "$lastTag".. "$srcPath" >> "$tempFile"
 ${VISUAL:-${EDITOR:-vi}} "$tempFile"
@@ -32,11 +37,30 @@ git commit -m "Update change log" "$changeLog" 1>&2
 ## Update install.rdf
 ##############
 installRDF="$srcPath/install.rdf"
-sed -i.bak "s/<em:version>[^<]*<\/em:version>/<em:version>$version<\/em:version>/" "$installRDF"
+
+minVersion="`grep -o -P -e '(?<=<em:minVersion>)[^<]+' $installRDF`"
+newMinVersion=""
+read -p "Update minVersion? [$minVersion] " newMinVersion
+
+maxVersion="`grep -o -P -e '(?<=<em:maxVersion>)[^<]+' $installRDF`"
+newMaxVersion=""
+read -p "Update maxVersion? [$maxVersion] " newMaxVersion
+
+if [ -z "$newMinVersion"]; then
+	newMinVersion="$minVersion"
+fi
+if [ -z "$newMaxVersion"]; then
+	newMaxVersion="$maxVersion"
+fi
+
+sed -i.bak "s/<em:version>[^<]*/<em:version>$version/; \
+            s/<em:minVersion>[^<]*/<em:minVersion>$newMinVersion/; \
+            s/<em:maxVersion>[^<]*/<em:maxVersion>$newMaxVersion/" \
+    "$installRDF"
 # Some SED versions require an extension, but we don't need a backup
 rm "$installRDF.bak"
 
-git commit -m "Update version" "$installRDF" 1>&2
+git commit -m "Update versions in install.rdf" "$installRDF" 1>&2
 
 ##############
 ## Create tag
