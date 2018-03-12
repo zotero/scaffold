zotero/scaffold

commit aee21816dfbdd2d5fcfe63b37bb5934d2b24d392

Fix positioning of URL drop-down

src/chrome/content/scaffold/scaffold.js
@@ -886,6 +886,9 @@ var Scaffold = new function() {
 			listitem.setUserData("test-string", JSON.stringify(test), null);
 			listbox.appendChild(listitem);
 		}
+		
+		// Re-position URL drop-down
+		browserUrl.firstChild.position = 'after_start';
 	}
 
 	

commit a77732a66b0dfe59d20c25ec10afb28ca6d77bfb

:bug: Fix capitalization of browserURL
Introduced in aee21816dfbdd2d5fcfe63b37bb5934d2b24d392

This resulted to a code tab which was empty when loading
an existing translator.

src/chrome/content/scaffold/scaffold.js
@@ -888,7 +888,7 @@ var Scaffold = new function() {
 		}
 		
 		// Re-position URL drop-down
-		browserUrl.firstChild.position = 'after_start';
+		browserURL.firstChild.position = 'after_start';
 	}
 
 	