09290cfe1d800e4967b8a742f8b33729464f2a21
Philipp Zumstein <zuphilip@gmail.com>
2019-06-16 12:51:58 +0200

    Fix JSON parsing error because of final semicolon
    The `.trim()` will make sure that there is no line break,
    before we do the check for the final semicolon.

--- src/chrome/content/scaffold/scaffold.js
+++ src/chrome/content/scaffold/scaffold.js
@@ -671,7 +671,7 @@ var Scaffold = new function() {
 		var testEnd   = code.indexOf("/** END TEST CASES **/"); 
 		if (testStart !== -1 && testEnd !== -1) {
 			test = code.substring(testStart + 24, testEnd);
-			test = test.replace(/var testCases = /,'');
+			test = test.replace(/var testCases = /,'').trim();
 			// The JSON parser doesn't like final semicolons
 			if (test.lastIndexOf(';') == (test.length-1))
 				test = test.slice(0,-1);
